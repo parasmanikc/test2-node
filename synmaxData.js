efe16a02277a250b6fbcfebadeed7691f7f6a6d0
paras <parasmanikc@gmail.com>
2022-12-07 17:45:19 +0545

    first commit

---
+++ synmaxData.js
@@ -0,0 +1,9 @@
+const shipPositionOverTime = [
+    {lat: 10, lng: 20, t: 4},
+    {lat: 15, lng: 20, t: 1},
+    {lat: 15, lng: 30, t: 2},
+    {lat: 18, lng: 22, t: 3},
+    {lat: 18, lng: 25, t: 5}
+]
+
+module.exports = { shipPositionOverTime }
